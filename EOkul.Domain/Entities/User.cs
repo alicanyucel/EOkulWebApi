2ee3374f9e72960224de0b26b3cdba21eca0f7a3
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-07-08 20:48:45 +0300

    entities

---
+++ EOkul.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+public sealed class User : IdentityUser<Guid>
+{
+    public string FirstName { get; set; } = default;
+    public string LastName { get; set; } = default;
+    public string IdentityNumber { get; set; } = default;
+    public DateOnly BirtOfDate { get; set; }
+    public string? AvatarUrl { get; set; }
+}
