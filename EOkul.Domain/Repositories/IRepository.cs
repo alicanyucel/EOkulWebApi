c10b15dfbfd7d335070a6f77ae491d16e20c0506
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-07-08 21:36:18 +0300

    generic repository

---
+++ EOkul.Domain/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿
+
+using EOkul.Domain.Abstracts;
+using System.Linq.Expressions;
+
+namespace EOkul.Domain.Repositories;
+
+public interface IRepository<T> where T : Entity
+{
+    Task CreateAsync(T entity, CancellationToken cancellationToken = default);
+    void Update(T entity);
+    void Delete(T entity);
+    IQueryable<T> GetAll();
+    IQueryable<T> Where(Expression<Func<T, bool>> expression);
+    Task<T?> GetByIdAsync(Guid id,CancellationToken cancellationToken=default);
+    Task<bool> AnyAsync(Expression<Func<T, bool>> predicate,CancellationToken cancellationToken=default);
+}
